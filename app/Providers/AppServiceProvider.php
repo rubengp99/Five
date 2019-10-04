48e00d530e6628b5b5e9fc44afa082602eacc3d5
Ruben Garcia <rdgp99@gmail.com>
2019-10-04 04:45:54 -0400

    aw

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,7 +22,7 @@ class AppServiceProvider extends ServiceProvider
         
     public function boot()
     {
-        if(config('app.env') === 'production') {
+        if(config('app.env') === 'forum') {
             \URL::forceScheme('https');
         }
         Schema::defaultStringLength(191);
